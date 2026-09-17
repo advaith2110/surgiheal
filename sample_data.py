6efaaa699e79c08acb0fc2f19dda2e1b9691f70c
SurgiHeal <gadva@surgiheal.dev>
2026-09-17 20:59:07 +0530

    Set custom patient final recovery image

--- sample_data.py
+++ sample_data.py
@@ -33,7 +33,7 @@ PROGRESSION_IMAGE_MAP = {
     },
     "case_custom": {
         "day_1": PROGRESSION_DIR / "day_1_hip.jpg",
-        "final_day": PROGRESSION_DIR / "day_14_hip.jpg",
+        "final_day": PROGRESSION_DIR / "custom_final.jpg",
     },
 }
 
